b47f3fafa88bf9a569ab5d9efda36d3037807722
Keith Johnson kyj29 <kjohnson@cas.org>
2025-01-26 15:03:31 +0000

    Add initial configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "error",
+      { allowConstantExport: true },
+    ],
+    "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/naming-convention": "off",
+    "sort-imports": "off",
+    "sort-keys": "off",
+    "@typescript-eslint/prefer-readonly-parameter-types": "off",
+    "@typescript-eslint/no-magic-numbers": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+    },
+  ],
+};
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import type { ReactNode } from "react";
+
+export default function app(): ReactNode {
+  return (
+    <>
+      <h1>MAIN MENU</h1>
+      <button>characters</button>
+    </>
+  );
+}
